467a0d7358f44368117b26858a2d33295ef20bb7
Arya Ram <arya_ram@outlook.com>
2025-02-24 20:15:24 +0000

    Added GET, UPDATE and DELETE endpoints which all work

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GameListView
+from .views import GameListView, GameDetailView
 
 urlpatterns = [
-    path('', GameListView.as_view())
+    path('', GameListView.as_view()), 
+    path('<int:pk>/', GameDetailView.as_view())
 ]
--- game/views.py
+++ game/views.py
@@ -1,9 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Game
 from .serializers.common import GameSerializer
 
-# Create your views here.
 class GameListView(APIView):
 
     def get(self, request):
@@ -11,4 +11,44 @@ class GameListView(APIView):
         game_serialized = GameSerializer(game, many=True)
         return Response(game_serialized.data)
 
-    
+
+    def post(self, request):
+        serializer = GameSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, 201)
+        return Response(serializer.errors, 402)
+
+class GameDetailView(APIView):
+
+    def get(self, request, pk):
+        try:
+            game = Game.objects.get(pk=pk)
+        except Game.DoesNotExist:
+            raise NotFound(detail="Game not found.")
+        
+        game_serialized = GameSerializer(game)
+        return Response(game_serialized.data)
+
+
+    def put(self, request, pk):
+        try:
+            game = Game.objects.get(pk=pk)
+        except Game.DoesNotExist:
+            raise NotFound(detail="Game not found.")
+        
+        serializer = GameSerializer(game, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, 402)
+
+
+    def delete(self, request, pk):
+        try:
+            game = Game.objects.get(pk=pk)
+        except Game.DoesNotExist:
+            raise NotFound(detail="Game not found.")
+        
+        game.delete()
+        return Response(status=204)
