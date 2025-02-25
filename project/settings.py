9a600049e1b5bcd0a77b12f2be55021bfd7bbe70
Arya Ram <arya_ram@outlook.com>
2025-02-25 16:43:47 +0000

    fixed cors

--- project/settings.py
+++ project/settings.py
@@ -52,7 +52,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
