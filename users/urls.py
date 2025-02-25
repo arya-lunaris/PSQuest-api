b3b5e6aea23b5267af43ebc5174144f4a72a932d
Arya Ram <arya_ram@outlook.com>
2025-02-25 14:19:51 +0000

    added login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import SignupView, LoginView
 
 urlpatterns = [
-    path('signup/', RegisterView.as_view())
+    path('signup/', SignupView.as_view()),
+    path('login/', LoginView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotAuthenticated, ValidationError
 from django.contrib.auth import get_user_model
+import jwt
 from .serializers.common import UserSerializer
+from django.conf import settings
+from datetime import datetime, timedelta
+
 
 # Create your views here.
-class RegisterView(APIView):
+class SignupView(APIView):
 
     def post(self, request):
         serialized_user = UserSerializer(data=request.data)
@@ -15,6 +20,38 @@ class RegisterView(APIView):
     
 
 class LoginView(APIView):
-
+    
     def post(self, request):
-        return Response('HIT LOGIN ROUTE')
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        try:
+            User = get_user_model()
+            user = User.objects.get(username=username)
+
+            if not user.check_password(password):
+                raise ValidationError('Passwords do not match')
+            
+            exp_date = datetime.now() + timedelta(days=1)
+            
+
+            token = jwt.encode(
+                payload={
+                    'user': {
+                        'id': user.id,
+                        'username': user.profile_image,
+                        'is_admin': user.is_staff
+                    },
+                    'exp': int(exp_date.strftime('%s'))
+                },
+                key=settings.SECRET_KEY,
+                algorithm='HS256'
+            )
+
+            return Response({ 'message': 'Login was successful', 'token': token})
+
+        except (User.DoesNotExist, ValidationError) as e:
+            print(e)
+            raise NotAuthenticated('Invalid credentials')
+ 
+        
