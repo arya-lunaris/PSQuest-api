eba1ee9b94f82834196fe191d3a718feb108837e
Arya Ram <arya_ram@outlook.com>
2025-03-05 11:17:09 +0000

    set default message for email error

--- users/serializers/common.py
+++ users/serializers/common.py
@@ -5,6 +5,13 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False, allow_blank=True)
     password_confirmation = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if get_user_model().objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
     def validate(self, data):
         password = data.get('password')
         password_confirmation = data.get('password_confirmation')
@@ -29,7 +36,6 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         validated_data.pop('password_confirmation', None)
 
-        # Handle password update separately
         password = validated_data.get('password', None)
         if password:
             validated_data['password'] = hashers.make_password(password)
